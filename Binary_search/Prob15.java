53e6edf40f262fd0209ab9fc988d27403a706fe0
Arpan Mondal <arpan19991025@gmail.com>
2021-09-10 03:28:57 +0530

    Add Prob15

---
+++ Binary_search/Prob15.java
@@ -0,0 +1,23 @@
+// Check If N and Its Double Exist
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+         HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : arr){
+            if(! map.containsKey(num))
+                map.put(num, 1);
+            else{
+                map.put(num, map.get(num) + 1);
+            }
+        }
+        for(int num : arr){
+
+            if( num == 0 && map.containsKey(0)){
+               if(map.get(0) > 1)
+                    return true; 
+            }
+            else if( map.containsKey(2*num) )
+                return true;
+        }
+        return false;
+    }
+}
