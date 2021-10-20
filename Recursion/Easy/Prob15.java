c916ce2b64985f4afbcf6bbcbafef98602cdd3de
Arpan Mondal <arpan19991025@gmail.com>
2021-10-20 12:07:44 +0530

    Add prob15

---
+++ Recursion/Easy/Prob15.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if( n < 4 && n != 1)
+            return false;
+        else if( n == 1 )
+            return true;
+        else if( n % 4 != 0)
+            return false;
+        
+        return isPowerOfFour( n / 4 );
+    }
+}
